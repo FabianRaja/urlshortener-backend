9b32f57063adc1da4a79d27d26d7fdd54948467f
FabianRaja <fabiraja21052002@gmail.com>
2024-04-16 21:16:39 +0530

    modifications made

--- src/Authorization/auth.js
+++ src/Authorization/auth.js
@@ -23,15 +23,19 @@ export function isAuthorized(req,res,next){
 //custom authorization middleware for login alone
 export function isAuthorizedLogin(req,res,next){
     //getting headers 
-    const pass=req.headers["pass"];
+    const pass=req.headers["pass-token"];
     if(!pass){
         res.status(400).json({message:"Access denied"});
     }else{
         //comparing and verifying
         const check=pass==process.env.password;
+        console.log(pass,typeof(pass));
+        console.log(process.env.password,typeof(process.env.password))
         if(check==true){
+            console.log("passed",check)
             next();
         }else{
+            console.log("failed",check)
             res.status(400).json({message:"Invalid Admin"});
         }
     }
